623a0b069b9ae580571bd1da778fe9c5bc8ad8e9
Ben <bshukrallah@proton.me>
2023-12-16 16:03:33 -0500

    Hash table

---
+++ hash.py
@@ -0,0 +1,19 @@
+## Hash table should have a prime number of keys, ex: 2, 3, 5, 7, 11, 13, 17, 19, 23
+
+class HashTable:
+    def __init__(self, size = 7): # 7 would be index 0-6
+        self.data_map = [None] * size
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ": ", val)
+
+hashTable = HashTable()
+
+hashTable.print_table()
