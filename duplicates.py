6ec0c418e727550f15a4f5081bccc10c458acda9
Ben <bshukrallah@proton.me>
2023-12-19 22:34:36 -0500

    updated

--- duplicates.py
+++ duplicates.py
@@ -2,12 +2,12 @@ def find_duplicates(nums):
     new_list = {}
     for num in nums:
         if num in new_list:
-            new_list[num] = True
+            new_list[num] += 1
         else:
-            new_list[num] = False
+            new_list[num] = 1
     duplicates = []
     for num in new_list:
-        if new_list[num] == True:
+        if new_list[num] > 1:
             duplicates.append(num)
             
     return duplicates
