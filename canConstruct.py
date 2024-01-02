676c7d563456da2c5b76afd666458c1cccec41dc
Ben <bshukrallah@proton.me>
2024-01-01 20:34:40 -0500

    canConstruct

--- canConstruct.py
+++ canConstruct.py
@@ -9,11 +9,13 @@ def canConstruct(target, wordBank, memo=None):
         if target.find(word) == 0: #check if the "word" in the wordbank starts at the beginning of the target word
             result = target[len(word):]
             if (canConstruct(result, wordBank, memo) == True):
-                memo[result] = True
+                memo[target] = True
+                print(memo)
                 return True
             else:
                 memo[result] = False
 
+    memo[target] = False
     return False
             
 print(canConstruct("word", ["job", "rd", "wo", "li", "jk", "ing"]))
